48b65025bcaa3b803e0f339c32ddce6748f4b664
naruesornd <65111691+naruesornd@users.noreply.github.com>
2020-06-24 21:32:48 +0700

    task-8 added

---
+++ task-8/Ex 2.2.c
@@ -0,0 +1,74 @@
+/* Ex 2.2: Write a loop equivalent to the for loop above without using && or || */
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getline(char line[],int maxline);
+void copy(char to[],char from[]);
+
+
+int main()
+{
+    int len,max;
+    char line[MAXLINE], longest[MAXLINE];
+
+    max = 0;
+    while((len = getline(line,MAXLINE)) > 0)
+    {
+        if (len > max)
+        {
+            max = len;
+            copy(longest,line);
+        }
+    }
+    if (max > 0)
+        printf("Longest line: %s", longest);
+
+    return 0;
+}
+
+/* getLine function: read a line into s, return length */
+int getline(char s[], int lim)
+{
+    /* for loop from getline function in Chapter 1: for (i=0; i <lim-1 && (c=getchar())!=EOF && c!='\n ; ++i) */
+
+    int c, i;
+
+    c = 0;
+    i = 0;
+    while (i < lim-1)
+    {
+        c = getchar();
+        if (c == EOF)
+        {
+            lim = 0;
+        }
+        else if (c == '\n')
+        {
+            lim = 0;
+        }
+        else
+        {
+            s[i++] = c;
+        }
+    }
+
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[],char from[])
+{
+    int i;
+
+    i = 0;
+    while((to[i] = from[i]) != '\0')
+    ++i;
+}
---
+++ task-8/Ex 2.4.c
@@ -0,0 +1,83 @@
+/* Ex.2.4: Write an alternative version of squeeze(s1, s2) that deletes each character in s1 that matches any character
+ in the string s2 */
+
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+
+int getline(char line[],int maxline);
+void squeeze(char s1[], char s2[]);
+
+int main()
+{
+    char s1[MAXLINE], s2[MAXLINE];
+
+    printf("s1: ");
+    getline(s1, MAXLINE);
+
+    printf("s2: ");
+    getline(s2, MAXLINE);
+
+    squeeze(s1, s2);
+
+    printf("\nFunction squeeze in used: %s\n", s1);
+
+    return 0;
+}
+
+/* getLine function: read a line into s, return length */
+int getline(char s[], int lim)
+{
+    /* for loop from getline function in Chapter 1: for (i=0; i <lim-1 && (c=getchar())!=EOF && c!='\n ; ++i) */
+
+    int c, i;
+
+    i = 0;
+    c = 0;
+    while (i < lim-1)
+    {
+        c = getchar();
+        if (c == EOF)
+        {
+            lim = 0;
+        }
+        else if (c == '\n')
+        {
+            lim = 0;
+        }
+        else
+        {
+            s[i++] = c;
+        }
+    }
+
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* squeeze: delete each char in s1 which is in s2 */
+void squeeze(char s1[], char s2[])
+{
+    int i;       /* variable for s1 */
+    int j;       /* variable for s2 */
+    int k;       /* variable for storing i from s1 */
+
+    k = 0;
+
+    for (i = 0; s1[i] != '\0'; i++)    /* for initializing s1[i] */
+    {
+        for (j = 0; (s1[i] != s2[j]) && s2[j] != '\0'; ++j)  /* for checking each character in s2[j] against s1[i] character */
+        ;
+        if(s2[j] == '\0')   /* if s2 characters do not match any s1 characters, then this if statement works and s1[i] characters will be copied to s1[k] (resulting string), and if there is or are matches, if statement fails and s1[i] is not copied to s1[k] */
+        {
+            s1[k++] = s1[i];
+        }
+    }
+     s1[k] = '\0';
+}
